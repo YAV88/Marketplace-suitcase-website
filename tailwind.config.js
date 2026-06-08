3783e943a2aa74ebf497e3a2c72bf8d026d7c9d9
Alexey Yakovenko <113440696+YAV88@users.noreply.github.com>
2026-06-08 01:10:08 +0000

    fix style.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./*.html",
+content: [
+    "./index.html",
     "./*.js",
+    "./modules/**/*.js"
   ],
   darkMode: 'class',
   theme: {
